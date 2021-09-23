cc10324e5537effe1ee7e9b8179d511b14fcf377
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-09-23 00:17:46 -0500

    se crea el repositoria, se agregan la carpeta de Aprendizaje-gui, donde en su interior hay 2 carpetas, que corresponde a 2 proyectos.

---
+++ Aprendizaje-Guia/proyect-00/main.ts
@@ -0,0 +1,19 @@
+let saludo = 'Hellow World';
+
+console.log(saludo);
+console.log('Prueba');
+
+//Nota Importante:
+/*
+Para que el arci¿hivo de typeScript se complile automaticamente se debe colocar el siguiente código en la terminal:
+                                    tsc -w nomArchivo.ts
+
+colocar el siguiente comando para crear el archivo tsconfig.json, el cual reposa toda la configuración de typeScript
+                                    tsc --init
+
+Colocar el sigiente comando para que compile todo los archivos e typeScript:
+                                    tsc
+
+Colocar el siguiente comando para observar todos los archivos de typeScript
+                                    tsc -w
+*/
