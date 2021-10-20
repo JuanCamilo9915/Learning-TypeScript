881076d64ba3d764aa5b47b088cfbe50277578fc
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-10-20 14:12:22 -0500

    se configura el proyecto: proyect-09 con la librería de lite-server, y se agrega al archivo .gitignore, los modulos de Node JS.

---
+++ Aprendizaje-Guia/proyect-09/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/*
+Se configura este proyecto con lite-serve: https://github.com/johnpapa/lite-server
+previamente a este paso, se inicializa el proyecto como servidor con el sigte comando: npm init
+*/
+alert('Hellow Server');
---
+++ Aprendizaje-Guia/proyect-09/main.ts
@@ -0,0 +1,5 @@
+/*
+Se configura este proyecto con lite-serve: https://github.com/johnpapa/lite-server
+previamente a este paso, se inicializa el proyecto como servidor con el sigte comando: npm init
+*/
+alert('Hellow Server');
