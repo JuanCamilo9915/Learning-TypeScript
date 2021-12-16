7903bb53f1c73e074f4ea44edc135fc2f5591e78
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-16 01:04:44 -0500

    se realizaron pequqños cambios en el proyecto: proyectFinalCourse.

--- Aprendizaje-Guia/proyectsCursoCrehana/proyectFinalCourse/js/test.js
+++ Aprendizaje-Guia/proyectsCursoCrehana/proyectFinalCourse/js/test.js
@@ -1,3 +1,13 @@
 "use strict";
-//alert('Hellow TypeScript');
-console.log('Hellow TypeScript');
+//Seleccionando el Formulario del HTML-DOM
+var formulario = document.getElementById('form-gastos-personales');
+//Instanciando formData
+var formUsu = new FormData(formulario);
+//Recibiendo los Datos
+function recibirDatos() {
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert("detalle: ".concat(formUsu.get('detalle'), ",\n        Tipo Detalle: ").concat(formUsu.get('option'), ",\n        Valor: ").concat(formUsu.get('valor')));
+        //console.log(`Tipo Detalle: ${tipoDetalle.value as number}`);
+    });
+}
--- Aprendizaje-Guia/proyectsCursoCrehana/proyectFinalCourse/ts/test.ts
+++ Aprendizaje-Guia/proyectsCursoCrehana/proyectFinalCourse/ts/test.ts
@@ -1,2 +1,15 @@
-//alert('Hellow TypeScript');
-console.log('Hellow TypeScript');
+//Seleccionando el Formulario del HTML-DOM
+const formulario: HTMLFormElement = document.getElementById('form-gastos-personales') as HTMLFormElement;
+//Instanciando formData
+let formUsu = new FormData(formulario);
+
+//Recibiendo los Datos
+function recibirDatos() {
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert(`detalle: ${formUsu.get('detalle')},
+        Tipo Detalle: ${formUsu.get('option')},
+        Valor: ${formUsu.get('valor')}`);
+        //console.log(`Tipo Detalle: ${tipoDetalle.value as number}`);
+    });
+}
